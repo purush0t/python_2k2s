236f5ef2f78eab1aaf5078ea633e3bd7e2efdee8
purush0t <purush0thaman@proton.me>
2025-08-05 16:51:32 +0530

    handeled exceptions

--- file_sorter.py
+++ file_sorter.py
@@ -1,6 +1,7 @@
 import os
 import re
 import shutil
+import sys
 #folder path to execute the program
 wrk_path = "C:/Users/BIRYANI_31/Desktop/2024_kode/new_folder/"
 
@@ -27,8 +28,20 @@ ext = list(set(ext))
 #making directories in name of extension
 for g in ext:
     print(f"making directory name:{g}")
-    os.mkdir(g)
-    
+    # handling the exception file exists 
+    try:
+        os.mkdir(g)
+    except FileExistsError:
+        print(f"folder>{g}: already exists!")
+        #checks if the folder has  contents
+        if len(os.listdir(g))==0:
+            continue
+        #exits program if the folder has contents
+        else:
+            print(f"folder:{g} has contents")
+            print("exiting program...")
+            sys.exit(0)
+
 for r_n in f_names:
     for ex_t in ext:
         if r_n.split(".")[1] == ex_t:
